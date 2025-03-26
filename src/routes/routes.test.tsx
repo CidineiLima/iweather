2bb2d2b07a05fb0134483d49a8cdb52ec1d3aaa8
Cidinei-Lima <cidinei.lima@hotmail.com>
2025-03-26 17:42:20 -0300

    :recycle: refactor: Ajusta test Router

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,4 +1,5 @@
 import { act, screen, waitFor } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { Routes } from "@routes/index";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { render } from "@mocks/utils/customRender";
@@ -9,25 +10,6 @@ describe("Routes", () => {
   it("should be render Search screen when not city selecte", async () => {
     render(<Routes />);
     const title = await waitFor(() => screen.findByText(/^escolha um local/i));
-
-    expect(title).toBeTruthy();
-  });
-
-  it("shoul be render Dashboard screen when has city selected", async () => {
-    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
-
-    const city = {
-      id: "1",
-      name: "São Paulo",
-      latitude: 123,
-      longitude: 456,
-    };
-
-    await saveStorageCity(city);
-
-    await act(() => waitFor(() => render(<Routes />)));
-
-    const title = screen.getByText(city.name);
 
     expect(title).toBeTruthy();
   });
